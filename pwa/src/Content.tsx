fb6851e1a53bc4b22129fe542a98229e9769f08c
Mark West <66728126+MWest2020@users.noreply.github.com>
2022-06-22 13:39:51 +0200

    Initialize feature/GW-993/pip-template

---
+++ pwa/src/Content.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface ContentProps {
+  children: React.ReactNode;
+}
+
+export const Content: React.FC<ContentProps> = () => {
+  return <>Initialisation guide</>;
+};
